7829488d4c7017f47409e462b5b5f34a5a351efc
maciekwolus <maciekwolus@gmail.com>
2023-04-02 14:05:07 +0200

    Created moving into second window

--- chess_game/mainwindow.cpp
+++ chess_game/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +12,13 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    hide();
+    game_window = new GameWindow(this);
+    game_window->show();
 }
 
