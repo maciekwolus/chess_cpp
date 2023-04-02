ed518e6b74f75d9dc10b4300cf1e587411357eea
maciekwolus <maciekwolus@gmail.com>
2023-04-02 12:50:21 +0200

    File names change

--- chess_game/main.cpp
+++ chess_game/main.cpp
@@ -1,11 +1,11 @@
-#include "test_window.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    test_window w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
