fab4fcccc1c5d63f4ebf43d887def7fee6981f28
maciekwolus <maciekwolus@gmail.com>
2023-04-02 14:53:44 +0200

    Adding resources and game screen

--- chess_game/gamewindow.cpp
+++ chess_game/gamewindow.cpp
@@ -1,11 +1,18 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
+#include <QPixmap>
 
 GameWindow::GameWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+
+    //LOAD THE PICTURE OF BOARD
+    QPixmap board_pix(":/resources/img/board.png");
+    int w = ui->board_pic->width();
+    int h = ui->board_pic->height();
+    ui->board_pic->setPixmap(board_pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 GameWindow::~GameWindow()
